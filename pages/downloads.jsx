a6b51573cf33a3679b8e9743e47aad11f27430ce
mofos <isaacjohn2310@gmail.com>
2021-04-19 02:55:22 +0530

    final final commit
    bro this time pro level hogya hai website

--- pages/downloads.jsx
+++ pages/downloads.jsx
@@ -11,8 +11,8 @@ function downloads() {
 
             <div className={styles.a} style={{ overflowX: "hidden"}}>
                 <h1 className={styles.h1}>Get the WAP Attack Source Code</h1>
-                <p className={styles.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi odit sequi id nobis illum, culpa animi officia eligendi sed. Natus consequuntur corporis reprehenderit voluptate aliquam asperiores cumque, ullam accusamus in.</p>
-                <button className={styles.button}>Download WAP</button>
+                <p className={styles.p}>The VM is an instance of Kali Linux with Complete Setup of NODE, NPM and POSTGREsql. To Start Refer to the Setup Document Down Below.</p>
+                <button className={styles.button}><a href='https://drive.google.com/file/d/14J5zZSIus-tgCHIRQPnQ2zCKcASv4-bg/view?usp=sharing'target="_blank">Download WAP_VM</a></button>
                 </div>
                 <div className={styles.websiteimg}></div>
 
@@ -23,7 +23,7 @@ function downloads() {
             <div className={styles.a}>
 
                 <h1 className={styles.h11}>WAP ATTACK GUIDE</h1>
-                <p className={styles.p11}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi odit sequi id nobis illum, culpa animi officia eligendi sed. Natus consequuntur corporis reprehenderit voluptate aliquam asperiores cumque, ullam accusamus in.</p>
+                <p className={styles.p11}>a complete guide to cover all the Exercises. Beginner friendly for anyone interested in hacking and wants to take a step in Cyber Security </p>
                 <button className={styles.button}> <a href='/pdf/WAP'target="_blank">Download PDF</a></button>
                 </div>
                 <div className={styles.websiteimg11}></div>
@@ -35,7 +35,7 @@ function downloads() {
             <div className={styles.a}>
 
                 <h1 className={styles.h11}style={{color:"#DC143C;"}}>Linux Online Guide</h1>
-                <p className={styles.p11}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi odit sequi id nobis illum, culpa animi officia eligendi sed. Natus consequuntur corporis reprehenderit voluptate aliquam asperiores cumque, ullam accusamus in.</p>
+                <p className={styles.p11}>If you are Seeing this online Download this Guid to Linux to learn basics of terminal and Experiance what the PRO's do for a living with linux. Dont worry if you dont have linux install just go to home page and click Learn Linux or reffer the Guide to get started</p>
                 <button className={styles.button}> <a href='/pdf/linux'target="_blank">Download Linux Guide</a></button>
                 </div>
                 <div className={styles.websiteimg111}></div>
@@ -46,7 +46,7 @@ function downloads() {
             <div className={styles.a}>
 
                 <h1 className={styles.h11} style={{color:"#ffcc99;"}}>Manual Setup</h1>
-                <p className={styles.p11}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi odit sequi id nobis illum, culpa animi officia eligendi sed. Natus consequuntur corporis reprehenderit voluptate aliquam asperiores cumque, ullam accusamus in.</p>
+                <p className={styles.p11}>the brave ones may pass the test of time but with this in your download folder you may very well concour the world. A grate start is essential for a good day and with the setup manual you can have that with n errors (i guess?!).</p>
                 <button className={styles.button}> <a href='/pdf/setup'target="_blank">Download PDF</a></button>
                 </div>
                 <div className={styles.websiteimg11111}></div>
